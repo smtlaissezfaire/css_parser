c7b152d47a64ec7d0292f2aad2783adea9f2a604
Scott Taylor <scott@railsnewbie.com>
2008-07-16 00:18:11 -0400

    * Splitting off percentage

--- lib/css_parser.rb
+++ lib/css_parser.rb
@@ -4,6 +4,7 @@ require File.dirname(__FILE__) + "/css_parser/css_pre_parser"
 
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_color"
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_length"
+Treetop.load File.dirname(__FILE__) + "/css_parser/css_percentage"
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_time"
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_frequency"
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_angle"
