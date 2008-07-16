aad3d50a73a009a513967aed50278f09dea86c2d
Scott Taylor <scott@railsnewbie.com>
2008-07-16 00:05:19 -0400

    * Adding time

--- lib/css_parser.rb
+++ lib/css_parser.rb
@@ -1,6 +1,9 @@
 require "rubygems"
 require "treetop"
 require File.dirname(__FILE__) + "/css_parser/css_pre_parser"
+
+Treetop.load File.dirname(__FILE__) + "/css_parser/css_time"
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_frequency"
+
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_primitives"
 Treetop.load File.dirname(__FILE__) + "/css_parser/css"
