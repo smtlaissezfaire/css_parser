e6debdf3ccdbafe033212724f1c27a57f0ec1501
Scott Taylor <scott@railsnewbie.com>
2008-07-16 00:06:31 -0400

    * Split off angle

--- lib/css_parser.rb
+++ lib/css_parser.rb
@@ -4,6 +4,7 @@ require File.dirname(__FILE__) + "/css_parser/css_pre_parser"
 
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_time"
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_frequency"
+Treetop.load File.dirname(__FILE__) + "/css_parser/css_angle"
 
 Treetop.load File.dirname(__FILE__) + "/css_parser/css_primitives"
 Treetop.load File.dirname(__FILE__) + "/css_parser/css"
